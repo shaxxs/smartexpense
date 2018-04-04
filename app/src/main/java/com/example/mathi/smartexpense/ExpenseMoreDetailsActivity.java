6dbaa3132099f344eb3b0b10acb029230c33c65a
mathildepsn <mathilde.person@gmail.com>
2018-04-04 09:31:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mathi/smartexpense/ExpenseMoreDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.mathi.smartexpense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpenseMoreDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_more_details);
+    }
+}
