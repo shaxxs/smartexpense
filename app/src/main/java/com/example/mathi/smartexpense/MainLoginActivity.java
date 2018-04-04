e6dcfe22c3af8f5832afcf12bf25b63cb4f3bca8
mathildepsn <mathilde.person@gmail.com>
2018-04-04 17:20:53 +0200

    voyage
    ajout du libellé d'une destination
    
    ça a l'air sympa

--- app/src/main/java/com/example/mathi/smartexpense/MainLoginActivity.java
+++ app/src/main/java/com/example/mathi/smartexpense/MainLoginActivity.java
@@ -2,8 +2,19 @@ package com.example.mathi.smartexpense;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainLoginActivity extends AppCompatActivity {
+
+    private EditText inputLogin;
+    private EditText inputPassword;
+    private Button connexionButton;
+    private TextView forgotPasswordLink;
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
