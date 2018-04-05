b7d1cbd69a5798cbb1a65313aba3fead7eb845d6
Pierre40 <pgyejacquot@gmail.com>
2018-04-05 10:04:06 +0200

    class java pierre
    ajout des classes java manquantes
    
    ajout des classes java par layout à completer

---
+++ app/src/main/java/com/example/mathi/smartexpense/NewExpenseActivity.java
@@ -0,0 +1,13 @@
+package com.example.mathi.smartexpense;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newexpense);
+    }
+}
