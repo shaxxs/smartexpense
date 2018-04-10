ecfbc334bbfcc1787533465d18750dcc11c418be
Pierre40 <pgyejacquot@gmail.com>
2018-04-11 01:19:21 +0200

    MAJ connexion reussie
    connexion fonctionne avec msg toast / mauvais login pas géré
    
    maj du 10 avr 18

--- app/src/main/java/com/example/mathi/smartexpense/MainLoginActivity.java
+++ app/src/main/java/com/example/mathi/smartexpense/MainLoginActivity.java
@@ -1,29 +1,20 @@
 package com.example.mathi.smartexpense;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.example.mathi.smartexpense.network.HttpGetRequest;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import org.w3c.dom.Text;
-f5c171f324f326b7c3c1e605957e44bb923fc973
+import android.widget.Toast;
+
 
 public class MainLoginActivity extends AppCompatActivity {
     final String LOGIN_PASS_KEY = "user_profile";
@@ -41,7 +32,6 @@ public class MainLoginActivity extends AppCompatActivity {
         /* Gestion click sur le bouton */
         Button bouton = findViewById(R.id.btnConnexion);
         bouton.setOnClickListener(new View.OnClickListener() {
-            public static final String FILE_PROFILE = "file_user_profile";
 
             @Override
             public void onClick(View v) {
@@ -50,21 +40,31 @@ public class MainLoginActivity extends AppCompatActivity {
                 HttpGetRequest getRequest = new HttpGetRequest();
                 try {
                     result = getRequest.execute(myURL).get();
-                    //on ajoute les identifiants à notre fichier sharedpreferences
-                    SharedPreferences myPref = getSharedPreferences(FILE_PROFILE, Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = myPref.edit();
-                    editor.putString(LOGIN_PASS_KEY, result);
-                    editor.apply();
-                    Log.v("sharedpreferences", myPref.getString(LOGIN_PASS_KEY, "{}"));
-                    Intent intent = new Intent(MainLoginActivity.this, DashboardActivity.class);
-                    startActivity(intent);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Retour HTTPGetRequest : " + result);
-                try {JSONArray user = new JSONArray(result); } catch(JSONException e) {
+                    System.out.println("Retour HTTPGetRequest : " + result);
+                    if (result.equals("")){
+                        //afficher un message pour avertir de l'échec de la connexion
+                        Context context = getApplicationContext();
+                        CharSequence text = "Mot de Passe ou identifiant incorrect !";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }else {
+                        //on ajoute les identifiants à notre fichier sharedpreferences
+                        SharedPreferences myPref = getSharedPreferences(FILE_PROFILE, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = myPref.edit();
+                        editor.putString(LOGIN_PASS_KEY, result);
+                        editor.apply();
+                        Log.v("sharedpreferences", myPref.getString(LOGIN_PASS_KEY, "{}"));
+                        //afficher un message pour avertir de la réussite de la connexion
+                        Context context = getApplicationContext();
+                        CharSequence text = "Connexion Réussie";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                        Intent intent = new Intent(MainLoginActivity.this, DashboardActivity.class);
+                        startActivity(intent);
+                    }
+                } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
                 }
             }
